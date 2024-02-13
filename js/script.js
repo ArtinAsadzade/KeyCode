b0c686a6c70fd6844ed361fd2552f905aecb4b51
artin721 <artinasadzade7@gmail.com>
2024-02-13 14:15:12 +0330

    Key Code

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+let eventKey = $.getElementById('key')
+let eventLocation = $.getElementById('location')
+let eventWhich = $.getElementById('which')
+let eventAlt = $.getElementById('code')
+let keyCode = $.getElementById('keyCode')
+
+document.body.addEventListener('keydown', function (event) {
+    // event.preventDefault()
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+    eventKey.innerHTML = event.key;
+    eventLocation.innerHTML = event.location;
+    eventWhich.innerHTML = event.which;
+    eventAlt.innerHTML = event.code;
+    keyCode.innerHTML = event.which;
+})
